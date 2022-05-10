4b3554d0d542b87efe607dffc70fee99019371bc
azazia <azaziahmad9@gmail.com>
2022-05-10 11:09:07 +0800

    konfigurasi authentikasi

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,12 +41,34 @@ export default {
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
+    // https://auth.nuxtjs.org
+    '@nuxtjs/auth-next'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: 'http://backer-backend.buildwithangga.id/',
+  },
+
+  auth: {
+    strategies: {
+      local: {
+        endpoints: {
+          login: {
+              url: '/api/v1/sessions', 
+              method: 'post',
+              propertyName: 'data.token'
+             },
+          logout: false,
+          user: {
+              url: '/api/v1/users/fetch', 
+              method: 'get',
+              propertyName: 'data'
+            },
+        }
+      }
+    }
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
